13dfbf424cea19e24334c6d2611c61a323a899c7
abhi <abhishek.maheshwari1209@gmail.com>
2022-04-24 00:13:20 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,64 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () => {
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if (window.scrollY > 60) {
+    document.querySelector('#scroll-top').classList.add('active');
+  } else {
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+
+function loader() {
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+
+
+
+
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
